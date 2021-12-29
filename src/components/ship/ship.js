36ac1496795580aff29400ea3f5dd8d722e3d09a
Sh4ke96 <Sh4ke96@gmail.com>
2021-12-29 20:26:58 +0100

    add api information for ships component

--- src/components/ship/ship.js
+++ src/components/ship/ship.js
@@ -1,47 +1,20 @@
 import React from "react";
 import { Container } from "./ship.styled";
-import { ApolloClient, InMemoryCache, gql, useQuery } from "@apollo/client";
 
-const client = new ApolloClient({
-  uri: "https://api.spacex.land/graphql/",
-  cache: new InMemoryCache(),
-});
-
-const information = gql`
-  {
-    launchesPast(limit: 1) {
-      ships {
-        name
-        home_port
-        image
-        weight_kg
-      }
-    }
-  }
-`;
-
-function Ship() {
-  const { data, loading, error } = useQuery(information);
-  if (loading) return "Loading...";
-  if (error) return <pre>{error.message}</pre>;
-  console.log(data.launchesPast);
+function Ship(props) {
   return (
     <Container>
-      <img src={data.launchesPast[0].ships[0].image} alt="ship image" />
+      <img src={props.images} alt="ship image" />
       <div className="ship__information">
-        <h1 className="ship__name">{data.launchesPast[0].ships[0].name}</h1>
+        <h1 className="ship__name">{props.shipName}</h1>
         <div className="ship__details">
           <div className="ship__port">
             <h3 className="port__title">Home Port</h3>
-            <p className="port__name">
-              {data.launchesPast[0].ships[0].home_port}
-            </p>
+            <p className="port__name">{props.portTitle}</p>
           </div>
           <div className="ship__weight">
             <h3 className="weight__title">Weight [KG]</h3>
-            <p className="weight__value">
-              {data.launchesPast[0].ships[0].weight_kg}
-            </p>
+            <p className="weight__value">{props.shipWeight}</p>
           </div>
         </div>
       </div>
--- src/components/ships/ships.js
+++ src/components/ships/ships.js
@@ -1,14 +1,51 @@
 import React from "react";
 import { Container } from "./ships.styled";
 import Ship from "../ship/ship";
+import { ApolloClient, InMemoryCache, gql, useQuery } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://api.spacex.land/graphql/",
+  cache: new InMemoryCache(),
+});
+
+const information = gql`
+  {
+    launchesPast(limit: 1) {
+      ships {
+        name
+        home_port
+        image
+        weight_kg
+      }
+    }
+  }
+`;
 
 function Ships() {
+  const { data, loading, error } = useQuery(information);
+  if (loading) return "Loading...";
+  if (error) return <pre>{error.message}</pre>;
   return (
     <Container>
       <h2 className="ships__title">Rescue Ships</h2>
-      <Ship />
-      <Ship />
-      <Ship />
+      <Ship
+        images={data.launchesPast[0].ships[0].image}
+        portTitle={data.launchesPast[0].ships[0].home_port}
+        shipName={data.launchesPast[0].ships[0].name}
+        shipWeight={data.launchesPast[0].ships[0].weight_kg}
+      />
+      <Ship
+        images={data.launchesPast[0].ships[1].image}
+        portTitle={data.launchesPast[0].ships[1].home_port}
+        shipName={data.launchesPast[0].ships[1].name}
+        shipWeight={data.launchesPast[0].ships[1].weight_kg}
+      />
+      <Ship
+        images={data.launchesPast[0].ships[2].image}
+        portTitle={data.launchesPast[0].ships[2].home_port}
+        shipName={data.launchesPast[0].ships[2].name}
+        shipWeight={data.launchesPast[0].ships[2].weight_kg}
+      />
     </Container>
   );
 }
