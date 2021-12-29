f85af4ea50946f38fee867867dc24a2da5a4850a
Sh4ke96 <Sh4ke96@gmail.com>
2021-12-29 19:05:23 +0100

    fetch api with Apollo Client, add information about mission from API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import Card from "./components/card/card";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 function App() {
+  const client = new ApolloClient({
+    uri: "https://api.spacex.land/graphql/",
+    cache: new InMemoryCache(),
+  });
   return (
-    <div className="App">
-      <Card />
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <Card />
+      </div>
+    </ApolloProvider>
   );
 }
 
